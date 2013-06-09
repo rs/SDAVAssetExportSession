8c070163b52ecf8df2cfdc28e701de1f71d5b943
jallens <jallensxie@gmail.com>
2013-06-09 11:59:06 +0800

    Handle capture orientation in default video composition

--- SDAVAssetExportSession.m
+++ SDAVAssetExportSession.m
@@ -287,8 +287,42 @@
 
 	AVMutableVideoCompositionLayerInstruction *passThroughLayer = [AVMutableVideoCompositionLayerInstruction videoCompositionLayerInstructionWithAssetTrack:videoTrack];
 
+    // Handle capture orientation
+    UIImageOrientation videoAssetOrientation = UIImageOrientationUp;
+    BOOL isVideoAssetPortrait = NO;
+    CGAffineTransform videoTransform = videoTrack.preferredTransform;
+    if (videoTransform.a == 0 && videoTransform.b == 1.0 && videoTransform.c == -1.0 && videoTransform.d == 0)
+    {
+        videoAssetOrientation = UIImageOrientationRight;
+        isVideoAssetPortrait = YES;
+    }
+    if (videoTransform.a == 0 && videoTransform.b == -1.0 && videoTransform.c == 1.0 && videoTransform.d == 0)
+    {
+        videoAssetOrientation =  UIImageOrientationLeft;
+        isVideoAssetPortrait = YES;
+    }
+    if (videoTransform.a == 1.0 && videoTransform.b == 0 && videoTransform.c == 0 && videoTransform.d == 1.0)
+    {
+        videoAssetOrientation =  UIImageOrientationUp;
+    }
+    if (videoTransform.a == -1.0 && videoTransform.b == 0 && videoTransform.c == 0 && videoTransform.d == -1.0)
+    {
+        videoAssetOrientation = UIImageOrientationDown;
+    }
+    [passThroughLayer setTransform:videoTrack.preferredTransform atTime:kCMTimeZero];
+
 	passThroughInstruction.layerInstructions = @[passThroughLayer];
 	videoComposition.instructions = @[passThroughInstruction];
+
+    CGSize naturalSize;
+    if(isVideoAssetPortrait)
+    {
+        naturalSize = CGSizeMake(videoTrack.naturalSize.height, videoTrack.naturalSize.width);
+    }
+    else
+    {
+        naturalSize = videoTrack.naturalSize;
+    }
 
     return videoComposition;
 }
